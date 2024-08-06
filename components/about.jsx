e00499ad2d7e554aa7753970a1543d98315ebd5a
Holden Willett <htwillett8@gmail.com>
2024-08-06 16:22:56 -0500

    Changed About me

--- components/about.jsx
+++ components/about.jsx
@@ -9,8 +9,8 @@ export default function About (props) {
               <h1 className="mb-5 text-4xl">
               About <span className="font-bold">Me</span>
               </h1> 
-              <p className="my-5 text-lg lg:pr-24">Hey! Im Marc Bernardino, an undergraduate student at <span className="font-bold">Stanford University</span> majoring in Computer Science. My passions lie in creating experiences that bring real-world value to others. Whether it be through projects, research, or the industry, I strive to make an impact through my work. </p>
-              <p className="my-5 text-lg lg:pr-24">Currently, I&apos;&apos;e been working on Full-stack Web Development, Generative AI, and Machine Learning projects. I have also worked with Blockchain Technology.</p>
+              <p className="my-5 text-lg lg:pr-24">Hey! Im Holden Willett, an undergraduate student at <span className="font-bold">The University of Iowa</span> majoring in Computer Science and Engineering. I am passionate about software development and eager to expand my knowledge in hardware, particularly in embedded devices. I am very excited to learn and take on new challenges in the engineering field. </p>
+              <p className="my-5 text-lg lg:pr-24">I recently completed a highly rewarding internship at Caseys, where I served as a Software Engineer Intern on the Supply Chain team. Currently, I am back at school and continuing to work on various personal projects.</p>
               <h3 className="mb-2 text-l">Looking to reach me for work, research, or just to chat? I dont mind! Feel free to <a className="underline" href="#contact">contact</a> me!</h3>
               <h3 className="mb-2 italic text-[0.9rem]">What about my resume? <a className="underline" href="#contact">click me!</a></h3>
           </div>
